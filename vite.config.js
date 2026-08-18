c229823cf982e6aaa267399fa7d883703526e091
Gustavo <estrellaglm96@gmail.com>
2026-08-18 13:25:02 -0400

    Agregando realtor Recruitment V2

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/calculators/react/camilo',
-  //base: '/embededGeneral',
+  //base: '/calculators/react/camilo',
+  base: '/embededGeneral',
   define: {
     global: 'window'
   },
